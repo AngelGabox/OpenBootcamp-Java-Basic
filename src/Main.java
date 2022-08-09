4834b868915a87b6f47fe1df4bb04db36c13f23c
AngelGabriel2021 <agabrieldelaroca@gmail.com>
2022-08-09 14:01:56 -0500

    exercise_2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
+        System.out.println("Hello world!");
+        byte variable1 = 1;
+        System.out.println(variable1);
+        short variable2 = 10;
+        System.out.println(variable2);
+        int variable3 = 20;
+        System.out.println(variable3);
+        long variable4 = 10000;
+        System.out.println(variable4);
+        float variable5 = 5.5f;
+        System.out.println(variable5);
+        double variable6 = 10.5d;
+        System.out.println(variable6);
+        boolean variable7 = false;
+        System.out.println(variable7);
+        char variable8 = 'a';
+        System.out.println(variable8);
+        String variable9 = "Holle i am string";
+        System.out.println(variable9);
     }
 }
